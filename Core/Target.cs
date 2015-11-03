b0dc3fd87ca56d3d183b5b182c13871b4d8950a8
Pedro Reys <pedroreys@gmail.com>
2015-11-03 20:44:43 -0200

    Add repro for Assembly loading issue with Serilog

---
+++ Core/Target.cs
@@ -0,0 +1,18 @@
+﻿namespace Core
+{
+    using Serilog;
+    using Serilog.Context;
+
+    public class Target
+    {
+        public void DoSomething()
+        {
+            var logger = new LoggerConfiguration().WriteTo.ColoredConsole().CreateLogger();
+
+            using(LogContext.PushProperty("MyPropery", "foo"))
+            {
+                logger.Information("I CAN LOG");
+            }
+        }
+    }
+}
---
+++ CoreTest/ReproTests.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreTest
+{
+    using Core;
+
+    public class ReproTests
+    {
+        public void Should_not_blow_up()
+        {
+            var target = new Target();
+
+            target.DoSomething();
+        }
+    }
+}
